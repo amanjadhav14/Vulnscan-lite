f6afb77ff689b0af6c03d53b7b691e303598466f
Amanjadhav <aj5275828@gmail.com>
2026-05-25 14:02:06 +0530

    fixed regex cors

--- backend/main.py
+++ backend/main.py
@@ -43,7 +43,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex="https://.*vercel\.app",
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
